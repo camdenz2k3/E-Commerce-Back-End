a51674d3b3dca4da01c612c4c00c06cd0f241cd5
Snow Cactus <camdenz2k3@gmail.com>
2023-02-04 16:53:47 -0600

    finished routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -27,15 +27,37 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // create a new tag
+  try {
+    const newTag = await Tag.create(req.body)
+    res.json(newTag)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.put('/:id', async (req, res) => {
-  // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    res.json(updateTag)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete on tag by its `id` value
-});
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.json(deleteTag)
+  } catch (err) {
+    res.status(500).json(err)
+  }});
 
 module.exports = router;
