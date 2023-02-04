b8d199f26fac13f34621b0ee38861ec62adad7c8
Snow Cactus <camdenz2k3@gmail.com>
2023-02-04 16:40:41 -0600

    finished category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,15 +29,38 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // create a new category
+  try {
+    const newCategory = await Category.create(req.body)
+    res.json(newCategory)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+    res.json(updateCategory)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
+  try {
+    const deleteCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.json(deleteCategory)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
